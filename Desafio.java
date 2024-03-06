8bd361c77a6f60bb32180fe7f3bf7ef6eb56c7f7
Emanuel Carlos de Mello <87581111+EmanuelMello@users.noreply.github.com>
2024-03-06 17:08:09 -0300

    Create Questao 2

--- Desafio.java
+++ Desafio.java
@@ -18,6 +18,16 @@ public class Desafio {
         System.out.println("O valor da variável SOMA é: " + SOMA);
     }
 
+    // Questão 2
+    // Função para verificar se um número pertence à sequência de Fibonacci
+    public static boolean verificaFibonacci(int numero, List<Integer> fibonacci) {
+        for (int fib : fibonacci) {
+            if (fib == numero)
+                return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -25,6 +35,35 @@ public class Desafio {
         System.out.println("Questão 1:");
         questaoUm();
         System.out.println();
+
+        
+        // Questão 2
+        System.out.println("Questão 2:");
+         // Inicializa a sequência de Fibonacci com os dois primeiros números
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+
+        // Solicita ao usuário um número para verificar na sequência de Fibonacci
+        System.out.print("Informe um número para verificar na sequência de Fibonacci: ");
+        int numero = scanner.nextInt();
+
+        // Calcula e armazena os números da sequência de Fibonacci até encontrar o número fornecido
+        while (fibonacci.get(fibonacci.size() - 1) < numero) {
+            int ultimo = fibonacci.get(fibonacci.size() - 1);
+            int penultimo = fibonacci.get(fibonacci.size() - 2);
+            fibonacci.add(ultimo + penultimo);
+        }
+
+        // Verifica se o número fornecido pertence à sequência de Fibonacci
+        if (verificaFibonacci(numero, fibonacci)) {
+            System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
+            System.out.println("Sequência de Fibonacci até " + numero + ": " + fibonacci);
+        } else {
+            System.out.println("O número " + numero + " não pertence à sequência de Fibonacci.");
+            System.out.println("Sequência de Fibonacci até " + numero + ": " + fibonacci);
+        }
+
 
 
         scanner.close();
